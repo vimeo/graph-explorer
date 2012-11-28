e1b7aa398f17cf682dce739d590ed52bbb4703f3
Dieter Plaetinck <dieter@vimeo.com>
2012-11-27 19:18:17 -0500

    better graph of http timings and errors

--- graph_templates/swift_object.py
+++ graph_templates/swift_object.py
@@ -3,6 +3,7 @@ from graph_template import GraphTemplate
 class SwiftObjectTemplate(GraphTemplate):
     pattern       = "^stats.timers\.([^\.]+)\.object-server\.(.*)$"
     pattern_graph = "^stats.timers\.([^\.]+)\.object-server\.GET.timing.lower$"
+    http_methods = ['GET', 'HEAD', 'PUT', 'REPLICATE']
 
     def generate_targets(self, match):
         server = match.groups()[0]
@@ -17,16 +18,28 @@ class SwiftObjectTemplate(GraphTemplate):
 
     def generate_graphs(self, match):
         server = match.groups()[0]
-        name = 'swift-object-server-%s' % server
+        graphs = {}
 
+        name = 'swift-object-server-timings-%s' % server
         targets = []
-        for type in ['GET.timing.lower', 'GET.timing.upper', 'HEAD.timing.lower', 'HEAD.timing.upper',
-        'PUT.timing.lower', 'PUT.timing.upper', 'REPLICATE.timing.lower', 'REPLICATE.timing.upper']:
+        for method in self.http_methods:
+            for type in ('lower', 'upper_90'):
+                t = {}
+                t['name'] = '%s %s' % (server, type)
+                t['target'] = 'sumSeries(stats.timers.%s.object-server.%s.%s)' % (server, method, type)
+                targets.append(t)
+        graphs['tpl_' + name] = {'targets': targets}
+
+        name = 'swift-object-server-errors-%s' % server
+        targets = []
+        for method in self.http_methods:
             t = {}
-            t['name'] = '%s %s' % (server, type)
-            t['target'] = 'sumSeries(stats.timers.%s.object-server.%s)' % (server, type)
+            t['name'] = '%s %s' % (server, method)
+            t['target'] = 'stats_counts.%s.object-server.%s.errors' % (server, method)
             targets.append(t)
-        return {'tpl_' + name: {'targets': targets}}
+        graphs['tpl_' + name] = {'targets': targets}
+
+        return graphs
 
 
 # vim: ts=4 et sw=4:
