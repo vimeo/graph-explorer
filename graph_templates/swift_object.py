dba5b29dc2e58f95cdb26dbe4e8ee04db27a8f2a
Dieter Plaetinck <dieter@vimeo.com>
2012-11-28 14:32:16 -0500

    fix display of swift-object-server-timings graph

--- graph_templates/swift_object.py
+++ graph_templates/swift_object.py
@@ -22,13 +22,25 @@ class SwiftObjectTemplate(GraphTemplate):
 
         name = 'swift-object-server-timings-%s' % server
         targets = []
+        # tuples of a light with corresponding darker color
+        colors = {
+            'GET': ('#5C9DFF', '#375E99'), # blue
+            'HEAD': ('#FFFFB2', '#FFFF00'), # yellow
+            'PUT': ('#80CC80', '#009900'), # green 
+            'REPLICATE': ('#694C2E', '#A59482') # brown
+        }
         for method in self.http_methods:
             for type in ('lower', 'upper_90'):
                 t = {}
-                t['name'] = '%s %s' % (server, type)
-                t['target'] = 'sumSeries(stats.timers.%s.object-server.%s.%s)' % (server, method, type)
+                t['name'] = '%s %s' % (method, type)
+                t['target'] = 'sumSeries(stats.timers.%s.object-server.%s.timing.%s)' % (server, method, type)
+                color_key = 0 if type == 'lower' else 1
+                t['color'] = colors[method][color_key]
                 targets.append(t)
-        graphs['tpl_' + name] = {'targets': targets}
+        graphs['tpl_' + name] = {
+            'targets': targets,
+            'series': {'stack': False, 'lines': { 'show': True, 'lineWidth': 0.6, 'fill': False }}
+        }
 
         name = 'swift-object-server-errors-%s' % server
         targets = []
