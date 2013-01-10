ff23416ec4766c52f13b3b019f66ac61772338e5
Dieter Plaetinck <dieter@vimeo.com>
2013-01-10 11:19:27 -0500

    keeplastValue for swift metrics
    because often this data is not fed very often, and often there
    is no exception in the graphite schema for only these metrics
    usually you want to see gaps but here it makes sense to hide them
    it's your job to be aware when checks don't run often enough

--- plugins/swift.py
+++ plugins/swift.py
@@ -7,13 +7,15 @@ class SwiftPlugin(Plugin):
         {
             'match': '^servers\.(?P<server>[^\.]+)\.openstackswift\.(?P<category>container_metrics)\.(?P<account>[^\.]+)\.(?P<container>[^\.]+)\.(?P<type>[^\.]+)$',
             'default_group_by': 'type',
-            'target_type': 'count'
+            'target_type': 'count',
+            'configure': lambda self, match, target: {'target': 'keepLastValue(%s)' % match.string}
         },
         {
             'match': '^servers\.(?P<server>[^\.]+)\.openstackswift\.(?P<category>dispersion)\.?(?P<swift_type>container|object)?\.(?P<type>[^\.]+)$',
             'default_group_by': 'server',
             'default_graph_options': {'vtitle': 'events'},
-            'target_type': 'count'
+            'target_type': 'count',
+            'configure': lambda self, match, target: {'target': 'keepLastValue(%s)' % match.string}
         }
     ]
 
