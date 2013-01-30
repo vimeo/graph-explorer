0086b2d657e07fc6ae3a7739e29534ef980e7ee5
Dieter Plaetinck <dieter@vimeo.com>
2013-01-30 17:07:17 -0500

    implement sanitisation for swift proxy plugin

--- plugins/swift_proxy_server.py
+++ plugins/swift_proxy_server.py
@@ -5,25 +5,42 @@ class SwiftProxyServerPlugin(Plugin):
     http_methods = ['GET', 'HEAD', 'PUT', 'REPLICATE']
     targets = [
         {
-            'match': '^stats.timers\.(?P<server>[^\.]+)\.proxy-server\.(?P<swift_type>account|container|object)\.(?P<http_method>[^\.]+)\.(?P<http_code>[^\.]+)\.timing\.(?P<type>[^\.]+)$',
+            'match': '^stats.timers\.(?P<server>[^\.]+)\.proxy-server\.(?P<swift_type>account|container|object)\.(?P<http_method>[^\.]+)\.(?P<http_code>[^\.]+)\.timing\.(?P<what>[^\.]+)$',
             'default_graph_options': {'vtitle': 'duration in ms'},
             'target_type': 'timer'
         },
         {
-            'match': '^stats_counts\.(?P<server>[^\.]+)\.proxy-server\.?(?P<swift_type>account|container|object)?\.?(?P<http_method>[^\.]*)\.?(?P<http_code>[^\.]*)\.(?P<type>[^\.]+)$',
+            'match': '^stats_counts\.(?P<server>[^\.]+)\.proxy-server\.?(?P<swift_type>account|container|object)?\.?(?P<http_method>[^\.]*)\.?(?P<http_code>[^\.]*)\.(?P<wt>[^\.]+)$',
             'default_graph_options': {'vtitle': 'events seen in flushInterval'},
             'target_type': 'count'
         },
         {
-            'match': '^stats\.(?P<server>[^\.]+)\.proxy-server\.?(?P<swift_type>account|container|object)?\.?(?P<http_method>[^\.]*)\.?(?P<http_code>[^\.]*)\.(?P<type>[^\.]+)$',
+            'match': '^stats\.(?P<server>[^\.]+)\.proxy-server\.?(?P<swift_type>account|container|object)?\.?(?P<http_method>[^\.]*)\.?(?P<http_code>[^\.]*)\.(?P<wt>[^\.]+)$',
             'default_graph_options': {'vtitle': 'events/s'},
             'target_type': 'rate'
         },
     ]
 
+    def sanitize(self, target):
+        if 'wt' not in target['tags']:
+            return
+        sanitizer = {
+            'xfer': ('bytes', 'transferred'),
+            'errors': ('errors', None),
+            'handoff_count': ('handoffs', 'node'),
+            'handoff_all_count': ('handoffs', 'only hand-off locations'),
+            'client_disconnects': ('disconnects', 'client'),
+            'client_timeouts': ('timeouts', 'client')
+        }
+        wt = target['tags']['wt']
+        target['tags']['what'] = sanitizer[wt][0]
+        if sanitizer[wt][1] is not None:
+            target['tags']['type'] = sanitizer[wt][1]
+        del target['tags']['wt']
+
     def default_configure_target(self, target):
         m = target['tags'].get('http_method', '')
-        t = target['tags']['type']
+        t = target['tags'].get('type', '')
         if m == 'GET'       and t in ('lower', 'timeouts', 'xfer'): target['color'] = self.colors['blue'][0]
         if m == 'GET'       and t in ('upper_90', 'errors')       : target['color'] = self.colors['blue'][1]
         if m == 'HEAD'      and t in ('lower', 'timeouts', 'xfer'): target['color'] = self.colors['yellow'][0]
@@ -34,9 +51,5 @@ class SwiftProxyServerPlugin(Plugin):
         if m == 'REPLICATE' and t in ('upper_90', 'errors')       : target['color'] = self.colors['brown'][1]
         if m == 'DELETE'    and t in ('lower', 'timeouts', 'xfer'): target['color'] = self.colors['red'][0]
         if m == 'DELETE'    and t in ('upper_90', 'errors')       : target['color'] = self.colors['red'][1]
-        # 'xfer' is transferred bytes..
-        if target['tags']['type'] == 'xfer':
-            target['tags']['type'] = 'xfer_bytes'
-        return target
 
 # vim: ts=4 et sw=4:
