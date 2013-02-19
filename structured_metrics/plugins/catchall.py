03107bf5755a4674f0f19aab6d98492b56c2f837
Dieter Plaetinck <dieter@vimeo.com>
2013-02-19 14:00:54 -0500

    catchall doc update

--- structured_metrics/plugins/catchall.py
+++ structured_metrics/plugins/catchall.py
@@ -7,6 +7,7 @@ class CatchallPlugin(Plugin):
     Turns metrics that aren't matched by any other plugin in something a bit more useful (than not having them at all)
     Another way to look at it is.. plugin:catchall is the list of targets you can better organize ;)
     Note that the assigned tags (i.e. source tags) are best guesses.  We can't know for sure!
+    (this description goes for all catchall plugins)
     """
 
     targets = [
