b3d44599857f6d8754fa2748c268b4b5c21dcb57
Dieter Plaetinck <dieter@vimeo.com>
2013-02-22 10:25:02 -0500

    support running from anywhere

--- graph-explorer.py
+++ graph-explorer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python2
 
+import os
 from bottle import run, debug, PasteServer
 import config
+
+os.chdir(os.path.dirname(__file__))
 
 debug(True)
 run('app', reloader=True, host=config.listen_host, port=config.listen_port, server=PasteServer)
