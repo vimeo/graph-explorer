e646253ef38e931ed6ba3d57aedb6ef17a974915
Dieter Plaetinck <dieter@plaetinck.be>
2013-03-02 14:14:07 -0500

    clarify tag display related functions, add one to show title from tag list

--- assets/js/graph-explorer.js
+++ assets/js/graph-explorer.js
@@ -1,3 +1,5 @@
+tags_order_pre = ['what', 'type', 'target_type'];
+tags_order_post = ['server', 'plugin'];
 function display_tag(tag_name, tag_value) {
     // at some point it'll probably make sense to make the background color the inverse of the foreground color
     // cause there'll be so many foreground colors that are not always visible on any particular background.
@@ -5,7 +7,8 @@ function display_tag(tag_name, tag_value) {
 }
 function display_word(word) {
     // word can be anything. a misc. string, a tag, a <tag>:, !<tag>=, etc
-    // if it matches on a tag, display it correctly for that tag
+    // if it matches on a tag, display it correctly for that tag (colored label, key = parsed out tag, value the pattern)
+    // otherwise generic label
     orig = word;
     if (word.charAt(0) == '!') {
         word = word.slice(1);
@@ -16,21 +19,34 @@ function display_word(word) {
     }
     return "<span class='label'>" + orig + "</span>";
 }
+// build a label list (colored or regular) for patterns which may contain tags
 function generate_pattern_display(patterns) {
     var string = '';
     patterns.forEach(function(word) {string += display_word(word) });
     return string;
 }
-function generate_title_from(tags, order_pre, order_post) {
+// generate label list for tags k/v, ordered by moving tags based on keys in pre post lists to begin/end.
+function generate_title_from_dict(tags_dict, order_pre, order_post) {
     var title = '';
-    order_pre.forEach(function(tag) {if(tag in tags) {title += display_tag(tag, tags[tag]); }});
-    $.map(tags, function (tag_v,tag) {if($.inArray(tag, order_pre) < 0 && $.inArray(tag, order_post) < 0) {title += display_tag(tag,tag_v); }});
-    order_post.forEach(function(tag) {if(tag in tags) {title += display_tag(tag, tags[tag]); }});
+    order_pre.forEach(function(tag) {if(tag in tags_dict) {title += display_tag(tag, tags_dict[tag]); }});
+    $.map(tags_dict, function (tag_v,tag) {if($.inArray(tag, order_pre) < 0 && $.inArray(tag, order_post) < 0) {title += display_tag(tag,tag_v); }});
+    order_post.forEach(function(tag) {if(tag in tags_dict) {title += display_tag(tag, tags_dict[tag]); }});
     return title;
+}
+//same, but from a list of tag keys, where the value displayed is also the tag key
+function generate_title_from_list(tags_list, order_pre, order_post) {
+    var title = '';
+    order_pre.forEach(function(tag) {if($.inArray(tag, tags_list) >= 0) {title += display_tag(tag, tag); }});
+    tags_list.forEach(function(tag) {if($.inArray(tag, order_pre) < 0 && $.inArray(tag, order_post) < 0) {title += display_tag(tag,tag); }});
+    order_post.forEach(function(tag) {if($.inArray(tag, tags_list) >= 0) {title += display_tag(tag, tag); }});
+    return title;
+}
+function list_tags_in_graph_name_order(tags_list) {
+    return generate_title_from_list(tags_list, tags_order_pre, tags_order_post);
 }
 function get_graph_name(key, graph_data) {
     // set graph_name; with each tag in its own color. this way it's very clear how it's related to the query (esp. the group by)
-    var graph_name = generate_title_from(graph_data['constants_all'], ['what', 'type', 'target_type'], ['server', 'plugin']);
+    var graph_name = generate_title_from_dict(graph_data['constants_all'], tags_order_pre, tags_order_post);
     if(graph_name == "") {
         // this was probably a predefined graph, or at least one for which no constants are known
         graph_name = key;
