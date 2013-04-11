ae777ea16ed0dcfc4c73371931e17a98b1078b3d
Dieter Plaetinck <dieter@vimeo.com>
2013-04-11 18:23:22 -0400

    add stub for target_options preference

--- preferences.py
+++ preferences.py
@@ -1,6 +1,16 @@
 count_interval = 60  # counts are in buckets of <size>s seconds; i.e. statsd flushInterval
 timezone = "America/New_York"
-# match on graph properties and apply settings accordingly
+
+# match on target properties (before matching) and apply settings accordingly
+# this option doesn't actually work yet
+target_options = [
+        [
+            {'server': 'df.*'},
+            {'env': 'production'}
+        ]
+]
+
+# match on graph properties (after matching) and apply settings accordingly
 # all tags must match, if multiple tags are given in a list, they are OR'ed
 graph_options = [
         [
