b967ed0d3940726bc1ce6c5f398fa14383e404ab
Dieter Plaetinck <dieter@vimeo.com>
2013-10-22 16:55:07 -0400

    clarify elasticsearch host in config

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ listen_host = '0.0.0.0'  # defaults to "all interfaces"
 listen_port = 8080
 filename_metrics = 'metrics.json'
 log_file = 'graph-explorer.log'
-es_host = "es_host"
+es_host = "elasticsearch_host"
 es_port = 9200
