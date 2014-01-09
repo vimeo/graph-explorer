43546e04f440657dc1b53586f5482b04cd54f0bc
Dieter Plaetinck <dieter@vimeo.com>
2014-01-09 11:20:46 -0500

    nicer output

--- process_alerting.py
+++ process_alerting.py
@@ -13,15 +13,15 @@ config = make_config(config)
 db = alerting.Db(config.alerting_db)
 rules = db.get_rules()
 for rule in rules:
-    print "rule", rule
+    print("%80s" % rule),
     try:
         value = rule.get_value(config)
     except Exception, e:
         print "could not process:", e
         continue
-    print "value:", value
+    print("%15s" % value),
     status = rule.check(value)
-    print "check:", status
     msg_codes = ['OK', 'WARN', 'CRITICAL']
+    print("%10s" % msg_codes[status]),
     msg = "GE %s %s is %f" % (msg_codes[status], rule.expr, value)
     rule.notify_maybe(db, status, msg, config)
