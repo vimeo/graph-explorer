7dabe08f61ef7827fc7451a22785f84feafbf15e
Dieter Plaetinck <dieter@plaetinck.be>
2014-01-19 23:48:49 -0500

    better metrics (esp timers) for object-server

--- structured_metrics/plugins/swift_object_server.py
+++ structured_metrics/plugins/swift_object_server.py
@@ -5,15 +5,15 @@ class SwiftObjectServerPlugin(Plugin):
 
     targets = [
         {
-            'match': '^stats\.timers\.(?P<server>[^\.]+)\.object-server\.(?P<http_method>[^\.]+)\.timing\.(?P<what>[^\.]+)$',
-            'target_type': 'timer'
+            'match': '^stats\.timers\.(?P<server>[^\.]+)\.(?P<swift_type>object)-server\.(?P<http_method>[^\.]+)\.timing\.(?P<autosplit>[^\.]+)$',
+            'configure': lambda self, target: self.parse_statsd_timer(target)
         },
         {
-            'match': '^stats_counts\.(?P<server>[^\.]+)\.object-server\.?(?P<http_method>[^\.]*)\.(?P<what>async_pendings|errors|timeouts)$',
+            'match': '^stats_counts\.(?P<server>[^\.]+)\.(?P<swift_type>object)-server\.?(?P<http_method>[^\.]*)\.(?P<what>async_pendings|errors|timeouts)$',
             'target_type': 'count'
         },
         {
-            'match': '^stats\.(?P<server>[^\.]+)\.object-server\.?(?P<http_method>[^\.]*)\.(?P<what>async_pendings|errors|timeouts)$',
+            'match': '^stats\.(?P<server>[^\.]+)\.(?P<swift_type>object)-server\.?(?P<http_method>[^\.]*)\.(?P<what>async_pendings|errors|timeouts)$',
             'target_type': 'rate'
         }
     ]
