81b2d856b8b1f5734917bc884267abda97741285
dongweiming <ciici1234@hotmail.com>
2014-02-10 22:14:16 +0800

    refactor list_dashboards

--- dashboards.py
+++ dashboards.py
@@ -1,11 +1,8 @@
+import os
+import glob
+
+
 def list_dashboards():
-    import os
-    wd = os.getcwd()
-    os.chdir('templates/dashboards')
-    dashboards = []
-    for f in os.listdir("."):
-        if not f.endswith(".tpl"):
-            continue
-        dashboards.append(f[:-4])
-    os.chdir(wd)
+    tpl_list = glob.glob('templates/dashboards/*.tpl')
+    dashboards = map(lambda tpl: os.path.basename(tpl)[:-4], tpl_list)
     return sorted(dashboards)
