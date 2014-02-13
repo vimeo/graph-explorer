326206ed23fa132d4fdee863d24079df13eda227
Dieter Plaetinck <dieter@vimeo.com>
2014-02-13 15:32:47 -0500

    fix some tags

--- structured_metrics/plugins/udp.py
+++ structured_metrics/plugins/udp.py
@@ -10,13 +10,16 @@ class UdpPlugin(Plugin):
                     'configure': lambda self, target: self.fix_underscores(target, ['type', 'unit'])
                 },
                 {
-                    'match': '^servers\.(?P<server>[^\.]+)\.(?P<protocol>udp)\.(?P<type>[^\.]+)(?P<unit>Errors)$',
-                    'configure': lambda self, target: self.fix_underscores(target, ['type', 'unit'])
+                    'match': '^servers\.(?P<server>[^\.]+)\.(?P<protocol>udp)\.(?P<type>[^\.]+)Errors$',
+                    'configure': [
+                        lambda self, target: self.fix_underscores(target, 'type'),
+                        lambda self, target: self.add_tag(target, 'unit', 'Err/s')
+                    ]
                 },
                 {
                     'match': '^servers\.(?P<server>[^\.]+)\.(?P<protocol>udp)\.(?P<type>NoPorts)$',
                     'configure': [
-                        lambda self, target: self.add_tag(target, 'unit', 'Event'),
+                        lambda self, target: self.add_tag(target, 'unit', 'Event/s'),
                         lambda self, target: self.fix_underscores(target, 'type')
                     ]
                 }
