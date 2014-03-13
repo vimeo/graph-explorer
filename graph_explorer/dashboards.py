97528389000108e08c0e308b85da48430d917395
Pete Hollobon <phollobon@renshawbay.com>
2014-03-13 16:51:45 +0000

    Look for dashboards relative to package root

--- graph_explorer/dashboards.py
+++ graph_explorer/dashboards.py
@@ -3,6 +3,6 @@ import glob
 
 
 def list_dashboards():
-    tpl_list = glob.glob('templates/dashboards/*.tpl')
+    tpl_list = glob.glob(os.path.join(os.path.dirname(__file__),'templates/dashboards/*.tpl'))
     dashboards = map(lambda tpl: os.path.basename(tpl)[:-4], tpl_list)
     return sorted(dashboards)
