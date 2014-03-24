0987688cf7438e99ffea9fd852af972b6ffca605
Dieter Plaetinck <dieter@vimeo.com>
2014-03-24 14:28:11 -0400

    no windows line ends + PEP-8

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ class PyTest(TestCommand):
         TestCommand.finalize_options(self)
         self.test_args = []
         self.test_suite = True
+
     def run_tests(self):
         #import here, cause outside the eggs aren't loaded
         import pytest
@@ -17,14 +18,14 @@ class PyTest(TestCommand):
 
 
 setup_params = dict(
-    setup_requires=["setuptools_git >= 0.3",],
+    setup_requires=["setuptools_git >= 0.3", ],
     name="graph-explorer",
     description="Graph-Explorer",
     packages=find_packages(),
     scripts=glob("bin/*.py"),
     install_requires=[
         "bottle==0.11.6",
-        "urllib3==1.7.1", # 71f84f9ab29b9a2818a138fa000bcf75bb05812a
+        "urllib3==1.7.1",  # 71f84f9ab29b9a2818a138fa000bcf75bb05812a
         "elasticsearch==0.4.3",  # 422d43f4f8c981a5d93b06eefc3ce8f261cfb48c
         "paste==1.7.5.1",
         "wtforms",
