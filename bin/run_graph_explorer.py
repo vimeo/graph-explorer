e639d14ab370fa7f1393a320e23712eb6fe1eab2
Pete Hollobon <phollobon@renshawbay.com>
2014-04-02 11:37:49 +0100

    Move configuration to a non-Python config file, specified on the commandline when starting Graph Explorer

--- bin/run_graph_explorer.py
+++ bin/run_graph_explorer.py
@@ -2,27 +2,40 @@
 
 import os
 import sys
+from argparse import ArgumentParser
 from bottle import run, debug, PasteServer
 from graph_explorer import config
 from graph_explorer.validation import ConfigValidator
 
-c = ConfigValidator(obj=config)
-if not c.validate():
-    print "there's a problem with your configuration:"
-    for (key, err) in c.errors.items():
-        print key,
-        for e in err:
-            print "\n    ", e
-    sys.exit(1)
+
+def main():
+    parser = ArgumentParser(description="Run Graph Explorer")
+    parser.add_argument("configfile", metavar="CONFIG_FILENAME", type=str)
+    parser.add_argument("--debug", type=bool)
+    args = parser.parse_args()
+
+    config.init(args.configfile)
+    
+    c = ConfigValidator(obj=config)
+    if not c.validate():
+        print "Configuration errors (%s):" % args.configfile
+        for (key, err) in c.errors.items():
+            print key,
+            for e in err:
+                print "\n    %s" % e
+        sys.exit(1)
+
+    app_dir = os.path.dirname(__file__)
+    if app_dir:
+        os.chdir(app_dir)
+
+    debug(args.debug)
+    run('graph_explorer.app',
+        reloader=True,
+        host=config.listen_host,
+        port=config.listen_port,
+        server=PasteServer)
 
 
-app_dir = os.path.dirname(__file__)
-if app_dir:
-    os.chdir(app_dir)
-
-debug(True)
-run('graph_explorer.app',
-    reloader=True,
-    host=config.listen_host,
-    port=config.listen_port,
-    server=PasteServer)
+if __name__ == "__main__":
+    sys.exit(main())
